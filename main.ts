49b4808385b3cb1ef46bf613d2810fadc5e75e1f
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-04-20 09:13:09 +0200

    orjfo3rghi

--- main.ts
+++ main.ts
@@ -1,20 +1,30 @@
-basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 80)
-    basic.pause(500)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 80)
-    basic.pause(500)
-    maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(100)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-    basic.pause(1000)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
-    basic.pause(1000)
-    maqueen.motorStop(maqueen.Motors.M2)
-    basic.pause(100)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    basic.pause(1000)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
-    basic.pause(1000)
-    maqueen.motorStop(maqueen.Motors.M1)
-    basic.pause(100)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 80)
+    }
+    if (receivedNumber == 2) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 80)
+    }
+    if (receivedNumber == 3) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+    if (receivedNumber == 4) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    }
+    if (receivedNumber == 5) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+    }
+    if (receivedNumber == 6) {
+        maqueen.motorStop(maqueen.Motors.M2)
+    }
+    if (receivedNumber == 7) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    }
+    if (receivedNumber == 8) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+    }
+    if (receivedNumber == 9) {
+        maqueen.motorStop(maqueen.Motors.M1)
+    }
 })
+radio.setGroup(5555)
